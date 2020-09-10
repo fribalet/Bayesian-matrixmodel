e4cf150b6173f61175cf17727d13a8d75513eb2b
Paul home desktop <paulmattern@gmail.com>
2020-09-10 14:54:56 -0700

    renamed count to abundance in script

--- data/add_zinser_countdata.py
+++ data/add_zinser_countdata.py
@@ -9,12 +9,13 @@ zinser_data = pd.read_csv('Zinser_Figure2A.csv')
 
 #zinser_counts = zinser.values[:,1].astype(int) # cells A column
 #zinser_counts = zinser.values[:,2].astype(int) # cells B column
-zinser_counts = np.mean(zinser_data.values, axis=1).astype(int) # mean of both columns
+zinser_abundances = np.mean(zinser_data.values, axis=1).astype(int) # mean of both columns
 
 if len(sys.argv) > 1:
     with nc4.Dataset(sys.argv[1], 'a') as nc:
         # check time
-        assert nc.variables['time'].size == len(zinser_counts)
-        nc.createVariable('count', int, ('time',), fill_value=False)
-        nc.variables['count'][:] = zinser_counts
-        nc.variables['count'].long_name = 'cell counts across all size classes'
+        assert nc.variables['time'].size == len(zinser_abundances)
+        nc.createVariable('abundance', int, ('time',), fill_value=False)
+        nc.variables['abundance'][:] = zinser_abundances
+        nc.variables['abundance'].units = 'cells ml-1'
+        nc.variables['abundance'].long_name = 'cell abundance'
