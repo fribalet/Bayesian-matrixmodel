c12f8f8f7ab3382f0b5b5bf8df6a22ede5d21645
Paul home desktop <paulmattern@gmail.com>
2020-09-14 13:22:47 -0700

    updated script with abundance to count conversion for Zinser data

--- data/add_zinser_countdata.py
+++ data/add_zinser_countdata.py
@@ -15,7 +15,16 @@ if len(sys.argv) > 1:
     with nc4.Dataset(sys.argv[1], 'a') as nc:
         # check time
         assert nc.variables['time'].size == len(zinser_abundances)
-        nc.createVariable('abundance', int, ('time',), fill_value=False)
-        nc.variables['abundance'][:] = zinser_abundances
-        nc.variables['abundance'].units = 'cells ml-1'
-        nc.variables['abundance'].long_name = 'cell abundance'
+
+        if 'abundance' not in nc.variables:
+            nc.createVariable('abundance', int, ('time',), fill_value=False)
+            nc.variables['abundance'][:] = zinser_abundances
+            nc.variables['abundance'].units = 'cells ml-1'
+            nc.variables['abundance'].long_name = 'cell abundance'
+        
+        # For Zinser data, 20 microliter of sample was analyzed, so divide the cell abundance by the volume to determine how many cells were counted by the instrument.
+        if 'count' not in nc.variables:
+            nc.createVariable('count', int, ('time',), fill_value=False)
+            nc.variables['count'][:] = (np.round(zinser_abundances * 0.02)).astype(int) # 20 microliter = 0.02 milliliter 
+            nc.variables['count'].units = 'cells'
+            nc.variables['count'].long_name = 'cell count'
